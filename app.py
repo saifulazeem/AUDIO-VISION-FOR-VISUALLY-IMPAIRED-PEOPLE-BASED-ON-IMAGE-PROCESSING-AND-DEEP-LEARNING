a5472a4dd9ded52b82e0936080e83b868fbcc381
MUHAMMAD SAIF UL AZEEM ABBASI <57259157+saifulazeem@users.noreply.github.com>
2020-04-27 22:31:39 +0500

    flask_code_updated_one

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import tensorflow as tf
 import flask
 import werkzeug
+import my_model as my_mod
+from objectdet.TensorFlow.models.research.object_detection import obj
+from objectdet.TensorFlow.models.research.object_detection.obj import obj_det
+
+#import cv2
+#import matplotlib.pyplot as plt
 
 app = flask.Flask(__name__)
 
@@ -10,7 +17,16 @@ def handle_request():
     filename = werkzeug.utils.secure_filename(imagefile.filename)
     print("\nReceived image File name : " + imagefile.filename)
     imagefile.save(filename)
-    return "Image Uploaded Sucessfuly"
+
+    hg=my_mod.model_indoor()
+    sd=obj.myvar
+    desr=obj_det()
+    dfg=sd+" "+hg+" "+desr
+    print(dfg)
+
+
+
+    return dfg
 
 
 app.run(host="0.0.0.0", port=5000, debug=True)
